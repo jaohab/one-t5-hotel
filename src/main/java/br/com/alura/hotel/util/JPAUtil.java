a0aa7ff9d5e4ab83d4c73a8f343646f5bafbb39e
joaohab <joao_hab@hotmail.com>
2023-08-26 16:05:34 -0300

    Organização

---
+++ src/main/java/br/com/alura/hotel/util/JPAUtil.java
@@ -0,0 +1,15 @@
+package main.java.br.com.alura.hotel.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+
+    private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("hotel");
+
+    public static EntityManager getEntityManager() {
+        return FACTORY.createEntityManager();
+    }
+    
+}
